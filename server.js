94e1d4784b32016d92ac2257a10384af28940df5
ashishsah1000 <ashishsah1000@gmail.com>
2017-09-20 07:05:16 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,13 +1,38 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool=require('pg').Pool;
+
 
 var app = express();
 app.use(morgan('combined'));
 
+var config = {
+    username : 'ashishsah1000',
+    database : 'ashishsah1000',
+    db : 'db.imad.hasura-app.io',
+    port :'5432',
+    password: 'db-ashishsah1000-96970'
+}
+
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+
+var pool =new Pool(config);
+app.get('/test', function(req,res) {
+pool.query('SELECT * FROM users',function(err,result){
+    if(err){
+        res.status(500).send(err.toString());
+    }
+    else{
+        res.send(JSON.stringify(result));
+    }
+}
+);
+
+});
+
 
 app.get('/ui/style.css', function(req, res) {
     res.sendFile(path.join(__dirname, 'ui', 'style.css'));
